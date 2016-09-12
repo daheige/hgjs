725e711547feb5bab43bab6c3d82bf0f7c65901f
daheige <zhuwei313@hotmail.com>
2016-09-12 11:12:09 +0800

    update 追加脚本至doc.body

--- notes/loadScript.js
+++ notes/loadScript.js
@@ -2,12 +2,14 @@
 function loadScript(url, callback){
     var doc = document;
     var myjs = doc.createElement("script");
-    myjs.type = "text/javamyjs";
+    // var myheader = doc.getElementsByTagName('head')[0] || doc.body;//建议用doc.body
+    var myheader = doc.body;
+    myjs.type = "text/javascript";
     if (myjs.readyState){ //IE
         myjs.onreadystatechange = function(){
             if (myjs.readyState == "loaded" || myjs.readyState == "complete"){
                 myjs.onreadystatechange = null;
-                typeof callback == 'function' && callback(); //建议判断是否是function然后执行回调
+                typeof callback == 'function' && callback(); //判断是否是function然后执行回调
             }
         };
     } else { //Others: Firefox, Safari, Chrome, and Opera
@@ -16,6 +18,5 @@ function loadScript(url, callback){
         };
     }
     myjs.src = url;
-    var myheader = doc.getElementsByTagName('head')[0];
-    (myheader || doc.body).appendChild(myjs);//追加到头部，建议追加在body后面
+    myheader.appendChild(myjs);//追加到头部，建议追加在body后面
 }
