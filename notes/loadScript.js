165951c72a127b75895d447da8ff1a0faa3c2fb8
daheige <zhuwei313@hotmail.com>
2016-09-24 23:29:23 +0800

    更新回调函数处理

--- notes/loadScript.js
+++ notes/loadScript.js
@@ -5,17 +5,19 @@ function loadScript(url, callback){
     // var myheader = doc.getElementsByTagName('head')[0] || doc.body;//建议用doc.body
     var myheader = doc.body;
     myjs.type = "text/javascript";
-    if (myjs.readyState){ //IE
-        myjs.onreadystatechange = function(){
-            if (myjs.readyState == "loaded" || myjs.readyState == "complete"){
-                myjs.onreadystatechange = null;
-                typeof callback == 'function' && callback(); //判断是否是function然后执行回调
-            }
-        };
-    } else { //Others: Firefox, Safari, Chrome, and Opera
-        myjs.onload = function(){
-            typeof callback == 'function' && callback();
-        };
+    if( typeof callback == 'function') { //回调函数处理
+        if (myjs.readyState) { //IE
+            myjs.onreadystatechange = function(){
+                if (myjs.readyState == "loaded" || myjs.readyState == "complete"){
+                    myjs.onreadystatechange = null;
+                    callback(); //判断是否是function然后执行回调
+                }
+            };
+        } else { //Others: Firefox, Safari, Chrome, and Opera
+            myjs.onload = function(){
+                callback();
+            };
+        }
     }
     myjs.src = url;
     myheader.appendChild(myjs);//追加到头部，建议追加在body后面
