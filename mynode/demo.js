ed646a49c0305e5f4ea975b9797b09a6e78a8314
daheige <zhuwei313@hotmail.com>
2016-10-22 21:57:46 +0800

    demo

---
+++ mynode/demo.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Created by daheige.
+ * Git   : @daheige
+ * Email : zhuwei313@hotmail.com
+ * Date  : 2016/10/22
+ * Time  : 21:39
+ */
+console.log(1);
+var s = 2;
+console.log(s);
