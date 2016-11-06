f98abf05a428fe9e04dc2c7939f225ebd4c1c89d
daheige <zhuwei313@hotmail.com>
2016-11-06 21:41:55 +0800

    图片静态文件的服务器处理

---
+++ mynode/myweb.js
@@ -0,0 +1,52 @@
+//简单的html,css,js，图片静态文件的服务器处理
+var http = require("http");
+var fs   = require("fs");
+var url  = require("url");
+var path = require("path");//和路径相关的模块path
+var server = http.createServer(function(req, res) {
+    //解析用户的url地址为对象
+    var pathname = url.parse(req.url).pathname;
+    if(pathname == "/favicon.ico") res.end();
+    if(pathname == "/") pathname = "/index.html";
+    //读取静态文件
+    var extname = path.extname(pathname); //得到拓展名
+    fs.readFile("./myweb" + pathname, function(err, data) {
+        if (err) {
+            res.writeHead(200, {
+                "Content-Type": "text/plain;charset=utf-8"
+            });
+            res.end("this page not found");
+        }
+        //获取文件的拓展名
+        var mine_type = getFileMime(extname);
+        res.writeHead(200,{"Content-Type":mine_type});//返回文件的响应头文件类型
+        res.write(data);
+        res.end();
+    });
+});
+server.listen(8000); //我们可以监听80端口，这样可以通过localhost来访问，不需要加端口号
+function getFileMime(extname){
+    switch(extname){
+        case ".html":
+            return "text/html";
+            break;
+        case ".jpg":
+            return "image/jpg";
+            break;
+        case ".png":
+            return "image/png";
+            break;
+        case ".gif":
+            return "image/gif";
+            break;
+        case ".css":
+            return "text/css";
+            break;
+        case ".js":
+            return "text/javascript";
+            break;
+        default:
+            return "text/plain";
+            break;
+    }
+}
---
+++ mynode/myweb/test/js/a.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var mytest = document.getElementById('mytest');
+    mytest.style.height = "100px";
+    mytest.style.width = "100%";
+    mytest.style.background = "#eee";
+    mytest.style.border = "1px solid red";
+
+}
